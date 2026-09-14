89bde4499f5791a097a95a9121312b8c15e197bb
fiansyahm31125 <fiansyahm31125@gmail.com>
2026-09-14 10:13:45 +0700

    add template engine

--- public/index.php
+++ public/index.php
@@ -3,14 +3,25 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+// Create Twig
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', function ($request, $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'home.html.twig', [
+        'name' => 'John',
+    ]);
 });
 
 $app->run();
